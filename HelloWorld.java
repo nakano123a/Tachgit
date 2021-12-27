196338293a2b4ccbaac0225107f07fccbc2c5520
Natthawat Hiranwong <644230057@webmail.npru.ac.th>
2021-12-27 09:47:12 +0700

    Second comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello IT");
+        System.out.println("Hello Natthawat");
+        System.out.println("Hello Hiranwong");
     }
 } 
